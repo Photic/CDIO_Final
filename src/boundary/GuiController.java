a505cbf402d33562458d6efe8585baa447dde21e
marcuschristiansen <marcuschristiansen@marcuss-mbp.eduroam.wireless.dtu.dk>
2018-01-05 11:15:11 +0100

    ppp

--- src/boundary/GuiController.java
+++ src/boundary/GuiController.java
@@ -179,6 +179,10 @@ public class GuiController {
 		
 	}
 	
+	public void visitJailMessege(Field field, Player p) {
+		gui.showMessage(p.getName() + ", du er på besøg i fængslet. Hyg dig!");
+	}
+	
 	
 	public void setOwnerText(Player p) {
 		
