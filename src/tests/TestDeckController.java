d3eb38c8c9d8d811d1f9503b5b737ef476860b38
Tobias Hagemann <32563473+TBHagemann@users.noreply.github.com>
2018-01-09 13:43:45 +0100

    slettet en test som ikke funker

--- src/tests/TestDeckController.java
+++ src/tests/TestDeckController.java
@@ -8,39 +8,7 @@ public class TestDeckController extends ConstructorForJUnit {
 
 	@Test
 	public void testChanceField() {
-		
-		// Test fungerer ikke. Lav evt en ny test
-		
-//		int recieveMoneyCount = 0, payMoneyCount = 0, antiJailCardCount = 0, positionCard = 0, nothing = 0;
-//		boolean test; 
-//		
-//		for (int i = 0; i < 10000; i++) {
-//			p.getAccount().setBalance(30000);
-//			p.setPosition(0);
-//			
-//			dc.chanceField(p, plist, gui, fc);
-//			
-//			if(p.getAccount().getBalance() > 30000 && p.getPosition() == 0) 
-//				recieveMoneyCount++;
-//			else if (p.getAccount().getBalance() < 30000 && p.getPosition() == 0)
-//				payMoneyCount++;
-//			else if (p.getAccount().getAmountOfCards() >= 0)
-//				antiJailCardCount++;
-//			else if (p.getPosition() != 0)
-//				positionCard++; 
-//			else
-//				nothing++;
-//		}
-//		
-//		if (recieveMoneyCount > 0 && payMoneyCount > 0 && antiJailCardCount > 0 && positionCard > 0) {
-//			test = true; 
-//		} else {
-//			test = false;
-//			} 
-//		
-//		assertTrue(test);
-//			
-		
+
 		
 		
 	}
