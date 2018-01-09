8333917bdbf36e51dde8df5c7af373e20c333734
Photic <s155854@student.dtu.dk>
2018-01-09 10:52:47 +0100

    Path rescourses

--- src/controller/GUIController.java
+++ src/controller/GUIController.java
@@ -26,7 +26,7 @@ public class GUIController {
 
 	public GUIController(TextReader tr) {
 		try {
-			this.description = tr.textFromFile("guiText");
+			this.description = tr.textFromFile("src/main/rsc/guiText");
 		} catch (IOException e) {
 			System.out.println("Something went wrong in the GUIController constructor");
 			e.printStackTrace();
