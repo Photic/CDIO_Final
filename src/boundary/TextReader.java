a9bb37c7a6b0570c87f1cd42dbf103d278067633
TheyCallMeOii <Stephankhader@gmail.com>
2018-01-10 11:04:07 +0100

    Kommentare - Textreader

--- src/boundary/TextReader.java
+++ src/boundary/TextReader.java
@@ -5,68 +5,85 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class TextReader {		
-		
-		public String[] textFromFile(String fileName) throws IOException {
-			String[] outputArray;
-		    
-		    FileReader fr = new FileReader(fileName);
-		    BufferedReader bf = new BufferedReader(fr);
-		    FileReader fileA = new FileReader(fileName);
-		    
-		    outputArray = new String[countArray(fileA)];
-		    
-		    try {
-		        int i=0;
-		        
-		        String line; 
-		      
-		        while((line = bf.readLine()) != null){
-		        	outputArray[i] = line;
-		            i++;
-		        }
-		        bf.close();
 
-		    } catch (IOException e) {
-		        e.printStackTrace();
-		    }
-		    
-			return outputArray;
-		}
+	
+	/**
+	 * Funktionen henter teksen fra en fil, og returnere det som et array, så det kan bruges andre steder i koden
+	 * 
+	 * @param fileName
+	 * @return output Array
+	 * @throws IOException
+	 */
+	
+	public String[] textFromFile(String fileName) throws IOException {
+		String[] outputArray;
+
+		//Filereader fylder sin buffer op med karaktere fra en given fil
+		FileReader fr = new FileReader(fileName);
 		
-		/**
-		 * Prints the array.
-		 * @param a
-		 */
-		public void printArray(String[] a) {
-		    for (int i = 0; i<a.length; i++) {
-		    System.out.println(a[i]);
-		    }
-		}
+		//BufferedReader indlæser fra en inputstream, som i det her tilfælde er en FileReader
+		BufferedReader bf = new BufferedReader(fr);
 		
 		
-		/**
-		 * Hjælpefunktion til main metoden.
-		 * @param fileName
-		 * @return
-		 * @throws IOException
-		 */
-		private int countArray(Reader fileName) throws IOException{
-			
-			BufferedReader tempFile = new BufferedReader(fileName);
-			
-			int arrayCount = 0;
-			String line = tempFile.readLine();
-			while(line!=null){
-				line = tempFile.readLine();
-				arrayCount++;
+		//return array instantieres med størelsen af hvor mange linjer tekst der er at hente ind fra filen
+		FileReader fileA = new FileReader(fileName);
+		outputArray = new String[countArray(fileA)];
+
+		//Try Catch bruges til enten at udføre funktionen eller give en besked uden at breake programmet
+		try {
+			int i=0;
+			String line; 
+
+			while((line = bf.readLine()) != null){
+				outputArray[i] = line;
+				i++;
 			}
-			
-			return arrayCount; 
-		}
-		
+			//Her lukkes BufferedReader manuelt, så javas trashcollecter ikke behøver gøre det 
+			bf.close();
 
-		
-		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return outputArray;
 	}
+
+	/**
+	 * Prints the array.
+	 * @param a
+	 */
+	 public void printArray(String[] a) {
+		for (int i = 0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
+	 }
+
+
+	 /**
+	  * Hjælpefunktion til main metoden. Metoden er til for at man ikke skal lave et array af en fast størrelse. 
+	  * Lige meget hvor stor en textfil man indlæser vil ovenstående metode returnere et array uden null på nogle af pladserne 
+	  * 
+	  * @param fileName
+	  * @return
+	  * @throws IOException
+	  */
+	 private int countArray(Reader fileName) throws IOException{
+
+		 BufferedReader tempFile = new BufferedReader(fileName);
+		 int arrayCount = 0;
+		 String line = tempFile.readLine();
+		 
+		 while(line!=null){
+			 line = tempFile.readLine();
+			 arrayCount++;
+		 }
+
+		 return arrayCount; 
+	 }
+
+
+
+
+}
 
 
