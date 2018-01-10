ad965e2580f3d0d5dd16e191d8040f7fbe25b3c4
Photic <s155854@student.dtu.dk>
2018-01-10 12:42:49 +0100

    LegeBranch Jail lyd virker.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 import controller.GameController;
 
@@ -8,9 +12,25 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		new GameController().gameControl();
-			
+		File jail = new File("Jail.wav");
+		playSound(jail);
 		
+		new GameController().gameControl();
+		
+	}
+	
+	static void playSound(File jail) {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(jail));
+			clip.start();
+			
+			Thread.sleep(clip.getMicrosecondLength()/1000);
+			
+		} catch (Exception e) {
+			System.out.println(e + " MY ASS");
+			// TODO: handle exception
+		}
 	}
 	
 }
