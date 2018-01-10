294773392201b6956ece571dc8e2f59b1685524e
Photic <s155854@student.dtu.dk>
2018-01-10 12:01:34 +0100

    Gameboard done 2.0

--- src/entity/gameboard/GameBoard.java
+++ src/entity/gameboard/GameBoard.java
@@ -47,54 +47,63 @@ public class GameBoard {
 		// Evert field in game.
 		this.fields = new Field[] 
 				{
-						new Start(fieldName[0], fieldDescription[0], standard,0),																			// 0
-						new Territory(fieldName[1], fieldDescription[1], blue, 1200, 1000, new int[] {50, 250, 750, 2250, 4000, 6000}, 1, 600),					// 1
+						new Start(fieldName[0], fieldDescription[0], standard,0),																				// 0
+						new Territory(fieldName[1], fieldDescription[1], blue, 1200, 1000, new int[] {50, 250, 750, 2250, 4000, 6000}, 1, 600),				// 1
 						new Chance(fieldName[2], fieldDescription[2], standard, 2),																			// 2
-						new Territory(fieldName[3], fieldDescription[3], blue, 1200,1000, new int[] {50, 250, 750, 2250, 4000, 6000}, 3, 600),						// 3
+						new Territory(fieldName[3], fieldDescription[3], blue, 1200,1000, new int[] {50, 250, 750, 2250, 4000, 6000}, 3, 600),					// 3
 						new Tax(fieldName[4], fieldDescription[4], standard, 4000, 4),																		// 4
-						new Shipping(fieldName[5], fieldDescription[5], standard, 4000, new int[] {500, 1000, 2000, 4000, 0, 0}, 5, 2000),							// 5
-						new Territory(fieldName[6], fieldDescription[6], pink, 2000, 1000, new int[] {100, 600, 1800, 5400, 8000, 11000}, 6, 1000),					// 6
+						new Shipping(fieldName[5], fieldDescription[5], standard, 4000, new int[] {500, 1000, 2000, 4000, 0, 0}, 5, 2000),						// 5
+						new Territory(fieldName[6], fieldDescription[6], pink, 2000, 1000, new int[] {100, 600, 1800, 5400, 8000, 11000}, 6, 1000),			// 6
 						new Chance(fieldName[7], fieldDescription[7], standard, 7),																			// 7
-						new Territory(fieldName[8], fieldDescription[8], pink, 2000, 1000, new int[] {100, 600, 1800, 5400, 8000, 11000}, 8, 1000),					// 8
-						new Territory(fieldName[9], fieldDescription[9], pink, 2400, 1000, new int[] {150, 800, 2000, 6000, 9000, 12000}, 9, 1200),					// 9
+						new Territory(fieldName[8], fieldDescription[8], pink, 2000, 1000, new int[] {100, 600, 1800, 5400, 8000, 11000}, 8, 1000),			// 8
+						new Territory(fieldName[9], fieldDescription[9], pink, 2400, 1000, new int[] {150, 800, 2000, 6000, 9000, 12000}, 9, 1200),			// 9
 						new Jail(fieldName[10], fieldDescription[10], standard, 10),																			// 10
-						new Territory(fieldName[11], fieldDescription[11], green, 2800, 2000, new int[] {200, 1000, 3000, 9000, 12500, 15000}, 11, 1400),			// 11
-						new Company(fieldName[12], fieldDescription[12], standard, 3000, 12, 1500),																	// 12
-						new Territory(fieldName[13], fieldDescription[13], green, 2800, 2000, new int[] {200, 1000, 3000, 9000, 12500, 15000}, 13, 1400),			// 13
-						new Territory(fieldName[14], fieldDescription[14], green, 3200, 2000, new int[] {250, 1250, 3750, 10000, 14000, 18000}, 14, 1600),			// 14
-						new Shipping(fieldName[15], fieldDescription[15], standard, 4000, new int[] {500, 1000, 2000, 4000, 0, 0}, 15, 2000),						// 15
-						new Territory(fieldName[16], fieldDescription[16], grey, 3600, 2000, new int[] {300, 1400, 4000, 11000, 15000, 19000}, 16, 1800),			// 16
+						new Territory(fieldName[11], fieldDescription[11], green, 2800, 2000, new int[] {200, 1000, 3000, 9000, 12500, 15000}, 11, 1400),		// 11
+						new Company(fieldName[12], fieldDescription[12], standard, 3000, 12, 1500),															// 12
+						new Territory(fieldName[13], fieldDescription[13], green, 2800, 2000, new int[] {200, 1000, 3000, 9000, 12500, 15000}, 13, 1400),		// 13
+						new Territory(fieldName[14], fieldDescription[14], green, 3200, 2000, new int[] {250, 1250, 3750, 10000, 14000, 18000}, 14, 1600),		// 14
+						new Shipping(fieldName[15], fieldDescription[15], standard, 4000, new int[] {500, 1000, 2000, 4000, 0, 0}, 15, 2000),					// 15
+						new Territory(fieldName[16], fieldDescription[16], grey, 3600, 2000, new int[] {300, 1400, 4000, 11000, 15000, 19000}, 16, 1800),		// 16
 						new Chance(fieldName[17], fieldDescription[17], standard, 17),																		// 17
-						new Territory(fieldName[18], fieldDescription[18], grey, 3600, 2000, new int[] {300, 1400, 4000, 11000, 15000, 19000}, 18, 1800),			// 18
-						new Territory(fieldName[19], fieldDescription[19], grey, 4000, 2000, new int[] {350, 1600, 4400, 12000, 16000, 20000}, 19, 2000),			// 19
+						new Territory(fieldName[18], fieldDescription[18], grey, 3600, 2000, new int[] {300, 1400, 4000, 11000, 15000, 19000}, 18, 1800),		// 18
+						new Territory(fieldName[19], fieldDescription[19], grey, 4000, 2000, new int[] {350, 1600, 4400, 12000, 16000, 20000}, 19, 2000),		// 19
 						new Parking(fieldName[20], fieldDescription[20], standard, 20),																		// 20
-						new Territory(fieldName[21], fieldDescription[21], red, 4400, 3000, new int[] {350, 1800, 5000, 14000, 17500, 21000}, 21, 2200),				// 21
+						new Territory(fieldName[21], fieldDescription[21], red, 4400, 3000, new int[] {350, 1800, 5000, 14000, 17500, 21000}, 21, 2200),		// 21
 						new Chance(fieldName[22], fieldDescription[22], standard, 22),																		// 22
-						new Territory(fieldName[23], fieldDescription[23], red, 4400, 3000, new int[] {350, 1800, 5000, 14000, 17500, 21000}, 23, 2200),				// 23
-						new Territory(fieldName[24], fieldDescription[24], red, 4400, 3000, new int[] {400, 2000, 6000, 15000, 18500, 22000}, 24, 2400),				// 24
-						new Shipping(fieldName[25], fieldDescription[25], standard, 4000, new int[] {500, 1000, 2000, 4000, 0, 0}, 25, 2000),						// 25
-						new Territory(fieldName[26], fieldDescription[26], white, 5200, 3000, new int [] {450, 2200, 6600, 16000, 19500, 23000}, 26, 2600),			// 26
-						new Territory(fieldName[27], fieldDescription[27], white, 5200, 3000, new int[] {450, 2200, 6600, 16000, 19500, 23000}, 27, 2600),			// 27
-						new Company(fieldName[28], fieldDescription[28], standard, 3000, 28, 1500),																	// 28
-						new Territory(fieldName[29], fieldDescription[29], white, 5600, 3000, new int[] {500, 2400, 7200, 17000, 20500, 24000}, 29, 2800),			// 29
+						new Territory(fieldName[23], fieldDescription[23], red, 4400, 3000, new int[] {350, 1800, 5000, 14000, 17500, 21000}, 23, 2200),		// 23
+						new Territory(fieldName[24], fieldDescription[24], red, 4400, 3000, new int[] {400, 2000, 6000, 15000, 18500, 22000}, 24, 2400),		// 24
+						new Shipping(fieldName[25], fieldDescription[25], standard, 4000, new int[] {500, 1000, 2000, 4000, 0, 0}, 25, 2000),					// 25
+						new Territory(fieldName[26], fieldDescription[26], white, 5200, 3000, new int [] {450, 2200, 6600, 16000, 19500, 23000}, 26, 2600),	// 26
+						new Territory(fieldName[27], fieldDescription[27], white, 5200, 3000, new int[] {450, 2200, 6600, 16000, 19500, 23000}, 27, 2600),		// 27
+						new Company(fieldName[28], fieldDescription[28], standard, 3000, 28, 1500),															// 28
+						new Territory(fieldName[29], fieldDescription[29], white, 5600, 3000, new int[] {500, 2400, 7200, 17000, 20500, 24000}, 29, 2800),		// 29
 						new GoToJail(fieldName[30], fieldDescription[30], standard, 30),																		// 30
-						new Territory(fieldName[31], fieldDescription[31], yellow, 6000, 4000, new int[] {550, 2600, 7800, 18000, 22000, 25000}, 31, 3000),			// 31
-						new Territory(fieldName[32], fieldDescription[32], yellow, 6000, 4000, new int[] {550, 2600, 7800, 18000, 22000, 25000}, 32, 3000),			// 32
+						new Territory(fieldName[31], fieldDescription[31], yellow, 6000, 4000, new int[] {550, 2600, 7800, 18000, 22000, 25000}, 31, 3000),	// 31
+						new Territory(fieldName[32], fieldDescription[32], yellow, 6000, 4000, new int[] {550, 2600, 7800, 18000, 22000, 25000}, 32, 3000),	// 32
 						new Chance(fieldName[33], fieldDescription[33], standard, 33),																		// 33
-						new Territory(fieldName[34], fieldDescription[34], yellow, 6400, 4000, new int[] {600, 3000, 9000, 20000, 24000, 28000}, 34, 3200), 			// 34
-						new Shipping(fieldName[35], fieldDescription[35], standard, 4000, new int[] {500, 1000, 2000, 4000, 0, 0}, 35, 2000),						// 35
+						new Territory(fieldName[34], fieldDescription[34], yellow, 6400, 4000, new int[] {600, 3000, 9000, 20000, 24000, 28000}, 34, 3200), 	// 34
+						new Shipping(fieldName[35], fieldDescription[35], standard, 4000, new int[] {500, 1000, 2000, 4000, 0, 0}, 35, 2000),					// 35
 						new Chance(fieldName[36], fieldDescription[36], standard, 36),																		// 36
-						new Territory(fieldName[37], fieldDescription[37], purple, 7000, 4000, new int[] {700, 3500, 10000, 22000, 26000, 30000}, 37, 3500),			// 37
+						new Territory(fieldName[37], fieldDescription[37], purple, 7000, 4000, new int[] {700, 3500, 10000, 22000, 26000, 30000}, 37, 3500),	// 37
 						new Tax(fieldName[38], fieldDescription[38], standard, 2000, 38),																		// 38
-						new Territory(fieldName[39], fieldDescription[39], purple, 8000, 4000, new int[] {1000, 4000, 1000, 28000, 34000, 40000}, 39, 4000)			// 39
+						new Territory(fieldName[39], fieldDescription[39], purple, 8000, 4000, new int[] {1000, 4000, 1000, 28000, 34000, 40000}, 39, 4000)	// 39
 				};
 		
 	}
 
+	/**
+	 * Gets a specific field in the gameboard.
+	 * @param a
+	 * @return
+	 */
 	public Field getField(int a) {
 		return fields[a];
 	}
 
+	/**
+	 * Gets the length of the gameboard.
+	 * @return
+	 */
 	public int getLength() {
 		return fields.length;
 	}
