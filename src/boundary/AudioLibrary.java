f03853af18c9cb086289a68439090ae442b1bbbc
Photic <s155854@student.dtu.dk>
2018-01-11 19:50:13 +0100

    Audio

--- src/boundary/AudioLibrary.java
+++ src/boundary/AudioLibrary.java
@@ -8,11 +8,12 @@ public class AudioLibrary {
 	 * Sounds to be used.
 	 */
 	private File jail;
+	private File move;
+	private File transaction;
+	private File birthday;
 
 	public AudioLibrary() {
-		
 		jail = new File("src/main/rsc/Jail.wav");
-
 	}
 	
 	public File getJailSound() {
