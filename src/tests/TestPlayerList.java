426f5cc3dc303f82cf76974b79f3b0970ed56959
TheyCallMeOii <Stephankhader@gmail.com>
2018-01-11 16:17:19 +0100

    Fjernet test "RemovePlayer" da vi ikke har metoden mere

--- src/tests/TestPlayerList.java
+++ src/tests/TestPlayerList.java
@@ -22,45 +22,5 @@ public class TestPlayerList {
 				assertEquals(expected, actual);
 			}
 	}
-
-	
-	@Test
-	public void testRemovePlayer(){
-		
-		boolean expected = true;
-		boolean actual = false; 
-		String[] plistPlayers = new String[]{"Tobias","Mathias","Stephan","Marcus","Steen","jacob"};
-		PlayerList plist = new PlayerList(6, plistPlayers);
-		
-		//Sætter bankrupt til true for player 1, 3 og 5
-		
-			plist.getPlayer(0).setBankrupt(true);
-			plist.getPlayer(1).setBankrupt(true);
-			plist.removePlayer(plist);
-
-	
-		if(plist.getPlayer(0).getName() == plistPlayers[2]){
-			actual = true;
-			assertEquals(expected, actual);
-			actual = false; 
-		}
-		if(plist.getPlayer(1).getName() == plistPlayers[3]){
-			actual = true;
-			assertEquals(expected, actual);
-			actual = false; 
-		}
-		if(plist.getPlayer(2).getName() == plistPlayers[4]){
-			actual = true;
-			assertEquals(expected, actual);
-			actual = false; 
-		}
-		if(plist.getPlayer(3).getName() == plistPlayers[5]){
-			actual = true;
-			assertEquals(expected, actual);
-			actual = false; 
-		}
-
-		
-	}
 	
 }
