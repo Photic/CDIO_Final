dbcc83e31c40d11fdc09e82576ae9ca22aa4a18d
Photic <s155854@student.dtu.dk>
2018-01-12 18:43:25 +0100

    audio

--- src/boundary/AudioLibrary.java
+++ src/boundary/AudioLibrary.java
@@ -18,63 +18,63 @@ public class AudioLibrary {
 		this.car = new File ("src/main/rsc/car.wav");
 		this.win = new File ("src/main/rsc/win.wav");
 		this.yay = new File ("src/main/rsc/yay.wav");
-		
+
 		this.dieThrow = new File[] {
 				new File ("src/main/rsc/die.wav"),
 				new File ("src/main/rsc/die2.wav"),
 				new File ("src/main/rsc/die3.wav")
-		};
-		
+				};
+
 		this.hello = new File[] {
 				new File ("src/main/rsc/hello.wav"),
 				new File ("src/main/rsc/hello2.wav"),
 				new File ("src/main/rsc/hello3.wav"),
 				new File ("src/main/rsc/hello4.wav")
-		};
+				};
 	}
-	
+
 	public File getJailSound() {
 		return this.jail;
 	}
-	
+
 	public File getMoveSound() {
 		return this.move;
 	}
-	
+
 	public File getDieSound(int n) {
 		return this.dieThrow[n];
 	}
-	
+
 	public int getDieLength() {
 		return this.dieThrow.length;
 	}
-	
+
 	public File getBirthday() {
 		return this.birthday;
 	}
-	
+
 	public File getCoinSound() {
 		return this.coin;
 	}
-	
+
 	public File getCarSound() {
 		return this.car;
 	}
-	
+
 	public File getHelloSound(int n) {
 		return this.hello[n];
 	}
-	
+
 	public int getHelloLenght() {
 		return this.hello.length;
 	}
-	
+
 	public File getWinSound() {
 		return this.win;
 	}
-	
+
 	public File getYaySound() {
 		return this.yay;
 	}
-	
+
 }
